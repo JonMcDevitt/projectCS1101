469c92569153a18c67429a0cf3f0e4aa9b4e372d
Lucas Ferreira <lucasferreiras@live.com>
2014-03-17 14:27:21 -0300

    Update Journals_Anthologies.java

--- Journals_Anthologies.java
+++ Journals_Anthologies.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
-public class Journals_Anthologies {
+public class Journals_Anthologies 
+{
 	
 	private String volume;
 	private String issue;
@@ -11,7 +12,8 @@ public class Journals_Anthologies {
 	{
 		
 	}
-	public Journals_Anthologies(String volume, String issue, String editor, String title, ArrayList<String> contributor){
+	public Journals_Anthologies(String volume, String issue, String editor, String title, ArrayList<String> contributor)
+	{
 		this.volume = volume;
 		this.issue = issue;
 		this.editor = editor;
