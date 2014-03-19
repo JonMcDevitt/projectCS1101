55e4854dc170720e7476e5d81b6e08257d2b8518
diegohkd <diegohkd@hotmail.com>
2014-03-19 14:16:23 -0300

    updated some basic stuffs

--- Magazine.java
+++ Magazine.java
@@ -4,69 +4,68 @@ public class Magazine {
 	private String ISBN;
 	private String title;
 	private String issue;
-	private String language; // enum???
-	private ArrayList<String> contributingWriters; // ArrayList??
-	public String languages;
-	
+	private String language; 
+	private ArrayList<String> contributingWriters; 
+
 	public Magazine()
 	{
-		
+
 	}
-	
+
 	public Magazine(String ISBN, String title, String issue, String language, ArrayList<String> contributingWriters)
 	{
 		this.ISBN = ISBN;
 		this.title = title;
 		this.issue = issue;
 		this.language = language;
-		this.contributingWriters = contributingWriters; // deep or shallow copy?
+		this.contributingWriters = contributingWriters;
 	}
-	
+
 	public void setISBN(String ISBN)
 	{
 		this.ISBN = ISBN;
 	}
-	
+
 	public void setTitle(String title)
 	{
 		this.title = title;
 	}
-	
+
 	public void setIssue(String issue)
 	{
 		this.issue = issue;
 	}
-	
+
 	public void setLanguage(String language)
 	{
 		this.language = language;
 	}
-	
+
 	public void setContributingWriters(ArrayList<String> contributingWriters)
 	{
 		this.contributingWriters = contributingWriters;
 	}
-	
+
 	public String getISBN()
 	{
 		return ISBN;
 	}
-	
+
 	public String getTitle()
 	{
 		return title;
 	}
-	
+
 	public String getIssue()
 	{
 		return issue;
 	}
-	
+
 	public String getLanguage()
 	{
 		return language;
 	}
-	
+
 	public String getContributingWriters()
 	{
 		return contributingWriters.toString();
