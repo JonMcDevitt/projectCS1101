d96c0928b7c0490ec1a2d1d63c25fa900edd36eb
diegohkd <diegohkd@hotmail.com>
2014-04-02 23:38:35 -0300

    add Frame class just to test JScrollPane
    This class creates a new window with lots of buttons using a scroll pane

--- Test.java
+++ Test.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 
 public class Test {
 	public static void main(String[] args)throws IOException, ParseException{
+		Frame f = new Frame();
 		User al = new User("Al", "A1i3n", "Random");
 		Book Teri = new Book("123-456-789-1234", "Random author", "Bridge to Teribithia", "3rd", "Fantasy", "English");
 		Newspapers news = new Newspapers("Chronicle", "02/02/02", "01/02/02");
