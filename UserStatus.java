15208fec96d759873d1ed3f4b5130e6d5a02ee50
Emadx <emad.mode@hotmail.com>
2014-04-16 14:46:06 -0300

    user status
    test it please

---
+++ UserStatus.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class UserStatus extends JFrame implements ActionListener
+{
+	private JPanel panel;
+	private JLabel curRes;
+   private JLabel remRes;
+	private JLabel curFine;
+	private JButton returnMain;
+	
+	public UserStatus()
+	{
+      int reser = SystemManager.getGeneralUser().getTotalOfReservations();
+		panel=new JPanel();
+		curRes=new JLabel("Current Reservations: "+reser);
+		remRes=new JLabel("Reservations Remaining: "+(reser - 8));
+		curFine=new JLabel("Current Fine: $0");
+		returnMain=new JButton("Return to Main");
+		returnMain.setBackground(Color.RED);
+		returnMain.setOpaque(true); //mac
+		returnMain.setBorderPainted(false);//mac
+		returnMain.addActionListener(this);
+      panel.add(curRes);
+		panel.add(remRes);
+		panel.add(curFine);
+		panel.add(returnMain);
+		add(panel);
+		
+		setTitle("User Status");
+		setSize(230,170);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()== returnMain)
+   		{
+   			System.out.println("return to main");
+	   	}
+	}
+   
+	public static void main(String[] args)
+	{
+		UserStatus gui=new UserStatus();
+	}
+   
+}
